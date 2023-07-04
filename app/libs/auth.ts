4c229cec9f2e254ed5768bd4c5dcb1ee08f2acf0
MaxShvachko <maxshvachko@ukr.net>
2023-07-04 21:01:39 +0300

    Change env names

--- app/libs/auth.ts
+++ app/libs/auth.ts
@@ -12,8 +12,8 @@ export const authOptions: AuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
     GitHubProvider({
-      clientId: process.env.GITHUB_ID as string,
-      clientSecret: process.env.GITHUB_SECRET as string
+      clientId: process.env.NEXT_PUBLIC_GITHUB_ID as string,
+      clientSecret: process.env.NEXT_PUBLIC_GITHUB_SECRET as string
     }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
