38814c786c844d35736db796b9f8397cda6ebb28
MaxShvachko <maxshvachko@ukr.net>
2024-08-12 22:59:38 +0300

    add ts ignore

--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -4,6 +4,8 @@ import L from 'leaflet';
 import { MapContainer, Marker, TileLayer } from 'react-leaflet';
 
 import 'leaflet/dist/leaflet.css';
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
 import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
 import markerIcon from 'leaflet/dist/images/marker-icon.png';
 import markerShadow from 'leaflet/dist/images/marker-shadow.png';
